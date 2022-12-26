12ebaac7fe545e808a55fd0048db9a252c2f1477
abbaskantov <104437381+abbaskantov@users.noreply.github.com>
2022-12-26 20:33:34 +0600

    add use cases to project

---
+++ app/src/main/java/com/example/shopinglist_app/domain/interactor/GetShopListUseCase.kt
@@ -0,0 +1,10 @@
+package com.example.shopinglist_app.domain.interactor
+
+import com.example.shopinglist_app.domain.ShopItem
+
+class GetShopListUseCase {
+
+     fun getShopList():List<ShopItem>{
+         TODO()
+    }
+}
