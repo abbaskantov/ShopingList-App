60a93906ade4d8be991ee39a71a56d1c5e98a352
abbaskantov <104437381+abbaskantov@users.noreply.github.com>
2022-12-26 23:55:58 +0600

    implementation of other classes domain

--- app/src/main/java/com/example/shopinglist_app/domain/interactor/GetShopItemUseCase.kt
+++ app/src/main/java/com/example/shopinglist_app/domain/interactor/GetShopItemUseCase.kt
@@ -3,8 +3,10 @@ package com.example.shopinglist_app.domain.interactor
 import com.example.shopinglist_app.domain.entity.ShopItem
 import com.example.shopinglist_app.domain.repository.Repository
 
-class AddShopItemUseCase(private val repository: Repository) {
-    fun addShopItem(shopItem: ShopItem){
-        repository.addShopItem(shopItem = shopItem)
+class GetShopItemUseCase(private val repository: Repository) {
+    fun getShopItem(shopItemId: Int): ShopItem {
+        return repository.getShopItem(shopItemId)
     }
+
+
 }
