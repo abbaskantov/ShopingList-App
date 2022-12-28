21aa1762a61caa6e75f7525c50ec24258c3129ea
abbaskantov <104437381+abbaskantov@users.noreply.github.com>
2022-12-28 17:55:04 +0600

    impl presentation

--- app/src/main/java/com/example/shopinglist_app/domain/interactor/GetShopListUseCase.kt
+++ app/src/main/java/com/example/shopinglist_app/domain/interactor/GetShopListUseCase.kt
@@ -1,10 +1,11 @@
 package com.example.shopinglist_app.domain.interactor
 
+import androidx.lifecycle.LiveData
 import com.example.shopinglist_app.domain.entity.ShopItem
 import com.example.shopinglist_app.domain.repository.Repository
 
 class GetShopListUseCase(private val repository: Repository) {
-     fun getShopList():List<ShopItem>{
+     fun getShopList():LiveData<List<ShopItem>>{
             return repository.getShopList()
     }
 }
---
+++ app/src/main/java/com/example/shopinglist_app/presentation/MainViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.shopinglist_app.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.shopinglist_app.data.RepositoryImpl
+import com.example.shopinglist_app.domain.entity.ShopItem
+import com.example.shopinglist_app.domain.interactor.*
+
+class MainViewModel:ViewModel() {
+
+    private val repository = RepositoryImpl
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val refactorShopItemUseCase = RefactorShopItemUseCase(repository)
+
+     val shopList = getShopListUseCase.getShopList()
+
+    fun deleteShopItem(shopItem: ShopItem){
+        deleteShopItemUseCase.deleteShopItem(shopItem = shopItem)
+    }
+
+    fun changeEnabledState(shopItem: ShopItem){
+        val newItem = shopItem.copy(enabled = !shopItem.enabled)
+        refactorShopItemUseCase.refactorShopItem(shopItem = shopItem)
+    }
+}
